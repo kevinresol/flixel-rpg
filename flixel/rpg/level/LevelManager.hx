6128b093c20eca8f462d1fea9b0b95899917c00f
KevinResoL <kevinresol@gmail.com>
2014-02-20 21:49:21 +0800

    Added current & currentName

--- flixel/rpg/level/LevelManager.hx
+++ flixel/rpg/level/LevelManager.hx
@@ -9,13 +9,21 @@ import flixel.rpg.core.RpgEngine;
 class LevelManager
 {
 	private var levels:Map<String, Level>;
+	
+	public var currentName(default, null):String;
+	public var current(default, null):Level;
 
 	public function new() 
 	{
 		levels = new Map<String, Level>();
 	}
 	
-	public function registerLevel(name:String, level:Level):Void
+	/**
+	 * Register a level to this manager. A level must be registered before it can be switched to.
+	 * @param	name
+	 * @param	level
+	 */
+	public function register(name:String, level:Level):Void
 	{
 		if (levels.exists(name))
 			FlxG.log.warn('LevelManager.registerLevel: Level name "$name" already exists');
@@ -23,14 +31,20 @@ class LevelManager
 		levels.set(name, level);
 	}
 	
-	public function switchLevel(name:String):Void
+	/**
+	 * Switch to a level
+	 * @param	name
+	 */
+	public function switchTo(name:String):Void
 	{
 		var level = levels.get(name);
 		
 		if (level == null)
 			throw 'LevelManager.switchLevel: Level name "$name" does not exist"';
 		
+		current = level;
+		currentName = name;
+		
 		RpgEngine.groups.registerLevel(level);
-	}
-	
+	}	
 }
